bf9fe5faa25d1bb43442e23087817d254a600fb9
Rohit Sangwan <rohitsangwan647@gmail.com>
2024-08-04 06:58:03 +0530

    windows dependency sync

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,11 +5,13 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   ble_peripheral
   screen_retriever
+  share_plus
   url_launcher_windows
   window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  mixin_logger
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
