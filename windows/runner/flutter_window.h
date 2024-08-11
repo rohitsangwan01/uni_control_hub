1fe680a010505b4ac5bacc439c707b9fb3bcfe1e
Rohit Sangwan <rohitsangwan647@gmail.com>
2024-08-11 12:17:43 +0530

    Listen to usb devices on Windows

--- windows/runner/flutter_window.h
+++ windows/runner/flutter_window.h
@@ -3,26 +3,40 @@
 
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
+#include <flutter/event_channel.h>
+#include <flutter/event_sink.h>
+#include <flutter/event_stream_handler_functions.h>
+#include <flutter/method_channel.h>
+#include <flutter/standard_method_codec.h>
 
 #include <memory>
-
 #include "win32_window.h"
+#include <flutter/method_channel.h>
 
 // A window that does nothing but host a Flutter view.
-class FlutterWindow : public Win32Window {
- public:
+class FlutterWindow : public Win32Window
+{
+public:
   // Creates a new FlutterWindow hosting a Flutter view running |project|.
-  explicit FlutterWindow(const flutter::DartProject& project);
+  explicit FlutterWindow(const flutter::DartProject &project);
   virtual ~FlutterWindow();
 
- protected:
+  void HandleMethodCall(
+      const flutter::MethodCall<flutter::EncodableValue> &call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+      
+  std::unique_ptr<flutter::BasicMessageChannel<flutter::EncodableValue>> messageConnector;
+
+  void OnUsbDeviceConnectionUpdate(bool connected);
+
+protected:
   // Win32Window:
   bool OnCreate() override;
   void OnDestroy() override;
   LRESULT MessageHandler(HWND window, UINT const message, WPARAM const wparam,
                          LPARAM const lparam) noexcept override;
 
- private:
+private:
   // The project to run.
   flutter::DartProject project_;
 
@@ -30,4 +44,4 @@ class FlutterWindow : public Win32Window {
   std::unique_ptr<flutter::FlutterViewController> flutter_controller_;
 };
 
-#endif  // RUNNER_FLUTTER_WINDOW_H_
+#endif // RUNNER_FLUTTER_WINDOW_H_
